9a0a7c3fbbd4975bfa7360146c0669553a099f28
daimessdn <daimessdn@gmail.com>
2020-07-29 10:18:42 +0700

    let's do also in js

---
+++ src/data.js
@@ -0,0 +1,69 @@
+const breads = [
+  {
+    tags  : ["intro"],
+    slides: [
+      {
+        title: "Hello",
+        contents: [
+          {type: "text", content: "This is a content"},
+          {type: "text", content: "This is another content"}
+        ]
+      },
+      {
+        title: "I like orange",
+        contents: [
+          {type: "text", content: "Orange is sweet"},
+          {type: "text", content: "and also orange is orange"}
+        ]
+      }
+    ],
+  },
+  {
+    tags  : ["story"],
+    slides: [
+      {
+        title: "Story about a duck",
+        contents: [
+          {type: "text", content: "Once upon a time, there was a duck swimming around the river. Suddenly, he met his friend."},
+          {type: "text", content: "He asked his friend to swim together and his friend agreed."}
+        ]
+      },
+      {
+        title: "",
+        contents: [
+          {type: "text", content: "Finally, they swam together."},
+          {type: "image", content: {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGDqyt_-PDDNCBbaURYMUI9h4MrEhwHUHygA&usqp=CAU",
+            alt: "duck swimming"
+          }}
+        ]
+      }
+    ],
+  },
+  {
+    tags  : ["code", "javascript"],
+    slides: [
+      {
+        title: "How to print first 5 numbers in JS",
+        contents: [
+          {type: "text", content: "1. Type the for loop:"},
+          {type: "code", content: "for (let i = 1; i <= 5; i++) {\n\n}"}
+        ]
+      },
+      {
+        title: "",
+        contents: [
+          {type: "text", content: "2. Let's apply <code>console.log()</code> inside the loop:"},
+          {type: "code", content: "for (let i = 1; i <= 5; i++) {\n\ \ console.log(i);\n}"}
+        ]
+      },
+      {
+        title: "",
+        contents: [
+          {type: "text", content: "3. Yay! We did it!!!"},
+          {type: "code", content: "1\n2\n3\n4\n5"}
+        ]
+      }
+    ],
+  },
+];
---
+++ src/main.js
@@ -0,0 +1,97 @@
+const loadCards = document.addEventListener("DOMContentLoaded", () => {
+let cardlist = document.getElementById("card-list");
+
+breads.forEach((bread) => {
+  let cards = "";
+  let breadTags = "";
+
+  bread.slides.forEach((slide) => {
+    let cardSlide = "";
+    let cardContent = "";
+
+    if (slide.title !== "") {
+      cardContent += `<h3>${slide.title}</h3>`;
+    }
+
+    
+    slide.contents.forEach((content) => {
+
+      let contentType = content.type;
+
+      switch (contentType) {
+        case "text":
+          cardContent += `<p>${content.content}</p>`;
+          break;
+        case "image":
+          cardContent += `<img src="${content.content.url}"
+                               alt="${content.content.alt}">`;
+          break;
+        case "code":
+          cardContent += `<pre><code>${content.content}</code></pre>`;
+          break;
+        default:
+          break;
+      }
+    });
+
+    cardSlide += `<div class="cards">${cardContent}</div>`;
+
+    cards += cardSlide;
+  });
+
+  bread.tags.forEach((tag) => {
+    breadTags += `<span class="tag">${tag}</span>`;
+  });
+
+  cardlist.innerHTML += `<div class="slides">
+                           ${cards}
+                           <div class="tags">
+                             ${breadTags}
+                           </div>
+                         </div>`;
+})
+});
+
+let currentSlide = 0;
+
+document.addEventListener("keydown", function(event) {    
+if (clicked !== undefined) {
+  if (event.key == "ArrowLeft") {
+    currentSlide++;
+  } else if (event.key == "ArrowRight") {
+    currentSlide--;
+  }
+}
+slideNav(currentSlide);
+});
+
+document.addEventListener('click', function(e) {
+let clicked = document.getElementById("clicked");
+if (clicked) {
+  currentSlide = 0;
+  slideNav(currentSlide);
+  clicked.removeAttribute("id");
+}
+
+e = e || window.event;
+var target = e.target || e.srcElement,
+text = target.textContent || target.innerText;
+
+if (target.className === "slides") {
+  target.id = "clicked";
+} else if (target.className === "cards") {
+  // console.log(target);
+  target.parentElement.id = "clicked";
+} else if (target.parentElement.className === "cards") {
+  target.parentElement.parentElement.id = "clicked";
+}
+}, false);
+
+const slideNav = (currentSlide) => {
+let motionHeight = currentSlide * 385;
+let clicked = document.getElementById("clicked").children;
+
+for (let i = 0; i < clicked.length; i++) {
+  clicked[i].style.transform = `translateY(${motionHeight}px)`;
+}
+}
